63aaa67a995aebfb5b99dd732a9de3d51aa182ae
Netfloex <38650595+Netfloex@users.noreply.github.com>
2019-11-09 10:43:59 +0100

    Lichte undo, ietsjes kleinere grootte van tekenen

--- site/teken/draw.js
+++ site/teken/draw.js
@@ -14,10 +14,23 @@ var gradient = c.createLinearGradient(0, 0, can.width, 0);
 gradient.addColorStop("0", "#667eea");
 gradient.addColorStop("1.0", "#764ba2");
 
+var undoImage = [c.getImageData(0, 0, can.width, can.height)]
+
+var i = 0
+var last = new Date()
+
 function loop() {
   requestAnimationFrame(loop)
   if (drawing.length<1) {
     return
+  }
+  if ((new Date()-drawing[drawing.length-1].time)>10) {
+    if ((new Date() - last)>100) {
+      console.log("Stopped Drawing");
+      undoImage.push(c.getImageData(0, 0, can.width, can.height))
+
+    }
+    last = new Date()
   }
   drawing.forEach(d=>{
     if ((new Date() - d.time)>400) {
@@ -33,7 +46,7 @@ function loop() {
   drawing.forEach(d=>{
     c.lineTo(d.x, d.y)
     c.strokeStyle = gradient
-    c.lineWidth = 100
+    c.lineWidth = 40
     c.stroke()
     // arc(d.x,d.y,10, "green")
   })
@@ -41,10 +54,21 @@ function loop() {
 }
 loop()
 
+addEventListener("keyup", function (e) {
+  if (e.key="z") {
+    if (undoImage.length<1) {
+      clear()
+      return
+    }
+    c.putImageData(undoImage.pop(), 0, 0);
+  }
+})
+can.addEventListener("mouseup", function (e) {
+  drawing = []
+  console.log("e");
+})
 var zt = new ZingTouch.Region(document.body);
-
 zt.bind(can, "pan", function(e){
-  console.log(e)
   var pos = e.detail.events[0]
   if (pos.x&&pos.y) {
     var cursor = {
