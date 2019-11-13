0ecdec6229f6d739fb1f08b5d6652e7b777ef30e
Netfloex <38650595+Netfloex@users.noreply.github.com>
2019-11-13 20:11:10 +0100

    Oribi

--- js/google-tag.js
+++ js/google-tag.js
@@ -5,3 +5,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-149926670-1', 'auto');
 ga('send', 'pageview');
+  (function(b,o,n,g,s,r,c){if(b[s])return;b[s]={};b[s].scriptToken="Xy0xNjAzNjg0NTcw";b[s].callsQueue=[];b[s].api=function(){b[s].callsQueue.push(arguments);};r=o.createElement(n);c=o.getElementsByTagName(n)[0];r.async=1;r.src=g;r.id=s+n;c.parentNode.insertBefore(r,c);})(window,document,"script","//cdn.oribi.io/Xy0xNjAzNjg0NTcw/oribi.js","ORIBI");
