a3f473273d6a92156add3e8d16b532831307ca77
Netfloex <38650595+Netfloex@users.noreply.github.com>
2019-11-15 19:59:32 +0100

    Veul beter voor mobiel, anders gaat hij overflowen enzo

--- js/messagerButton.js
+++ js/messagerButton.js
@@ -10,19 +10,31 @@ qi.b.addEventListener("click", function () {
 qi.c.addEventListener("click", function () {
   closeMessager()
 })
-
+function openMessager() {
+  // qi.w.classList.remove("hidden")
+  setTimeout(function () {
+    qi.w.classList.remove("hiddenMessager")
+    qi.b.classList.add("closeIcon")
+    qi.b.classList.remove("appear")
+  })
+  var src = new URL(qi.i.src)
+  if (src.pathname.length<2) {
+    qi.i.src = "/site/messager"
+  }
+}
 function closeMessager() {
   qi.w.classList.add("hiddenMessager")
   qi.b.classList.remove("closeIcon")
   qi.b.classList.add("appear")
+  setTimeout(function () {
+    // qi.w.classList.add("hidden")
+  },400)
 }
 function toggleMessager() {
-  qi.w.classList.toggle("hiddenMessager")
-  qi.b.classList.toggle("closeIcon")
-  qi.b.classList.toggle("appear")
-  var src = new URL(qi.i.src)
-  if (src.pathname.length<2) {
-    qi.i.src = "/site/messager"
+  if (qi.w.classList.contains("hiddenMessager")) {
+    openMessager()
+  } else {
+    closeMessager()
   }
 }
 
