330847caf0397ca816d01b46205e1c675527fc6c
Netfloex <38650595+Netfloex@users.noreply.github.com>
2019-11-17 17:17:00 +0100

    canvas.js, resize en schalen, standaard c en can vars

--- site/flappy-bird/bird.js
+++ site/flappy-bird/bird.js
@@ -1,33 +1,3 @@
-var can = d("#canvas")
-var c = can.getContext("2d")
-
-can.width = 1920
-can.height= 869
-
-function resize() {
-  var wrap = d("#wrap")
-  var gameArea = d('#wrap');
-  var widthToHeight = innerWidth/innerHeight
-  var newWidth = innerWidth;
-  var newHeight = innerHeight;
-  var newWidthToHeight = newWidth / newHeight;
-
-  if (newWidthToHeight > widthToHeight) {
-      newWidth = newHeight * widthToHeight;
-      gameArea.style.height = newHeight + 'px';
-      gameArea.style.width = newWidth + 'px';
-  } else {
-      newHeight = newWidth / widthToHeight;
-      gameArea.style.width = newWidth + 'px';
-      gameArea.style.height = newHeight + 'px';
-  }
-
-  gameArea.style.marginTop = (-newHeight / 2) + 'px';
-  gameArea.style.marginLeft = (-newWidth / 2) + 'px';
-}
-var running = true
-resize()
-addEventListener("resize", resize)
 var img = {}
 img.sand = newImage("sand")
 img.bird = newImage("bird")
