874f82c348161fd861e2b6ea26f331e067e2aec3
Netfloex <38650595+Netfloex@users.noreply.github.com>
2020-01-04 16:12:25 +0100

    Item animation niet op schaal

--- site/walker/items.js
+++ site/walker/items.js
@@ -5,7 +5,7 @@ Create.items = function () {
       return
     }
     c.save()
-      c.translate(i.x + player.pos.x, i.y + player.pos.y + Math.sin(i.rotation/10)*30)
+      c.translate(i.x + player.pos.x, i.y + player.pos.y + Math.sin(i.rotation/10)*ter.block.width*.3)
       rotate(i.rotation)
         image(i.img,-w/2,-w/2 , w, w)
     c.restore()
